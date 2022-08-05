33d6b863ab97d6f08620c4aa03baa04f9fafd536
Diogenes de Carvalho Pereira <diogenes.pereira@luizalabs.com>
2022-08-04 21:53:38 -0300

    feat: routes post/delete/put on cachorros

--- app.js
+++ app.js
@@ -26,8 +26,33 @@ let cachorros = [
 ]
 
 app.use(express.json())
+
 app.get('/cachorros', (req, res) => {
+    res.status(200).send(cachorros)
+})
+
+app.post('/cachorros', (req, res) => {
+    cachorros.push(req.body)
+    res.status(201).send(cachorros)
+})
+
+app.put('/cachorros/:id', (req, res) => {
+    let cachorro = cachorros.find(cach => cach.id == req.params.id)
+
+    if (!cachorro){
+        return res.status(400).json({ error: 'Usuário não encontrado.' });
+    }
+
+    cachorro.nome = req.body.nome
+    cachorro.raca = req.body.raca
+    cachorro.porte = req.body.porte
     res.send(cachorros)
+})
+
+app.delete('/cachorros/:id', (req, res) => {
+    let cachorro = cachorros.find(cach => cach.id == req.params.id)
+    cachorros.splice(cachorros.indexOf(cachorro), 1)
+    res.status(204).send(cachorros)
 })
 
 app.listen(3000, () => {
